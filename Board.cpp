bc9ebea4b9c0e2412daddc2380564970c2f801af
YosefSchwartz <73064850+YosefSchwartz@users.noreply.github.com>
2021-04-11 01:34:48 +0300

    Add files via upload

--- Board.cpp
+++ Board.cpp
@@ -8,34 +8,15 @@
 #include <ctime>
 using namespace std;
 
-#define RESET_VALUE -1
-
 namespace ariel {
     Board::Board(){
         map<unsigned int,map<unsigned int, char>> b;
-        unsigned int minH = numeric_limits<unsigned int>::max();
-        unsigned int minV = numeric_limits<unsigned int>::max();
-        unsigned int maxX = numeric_limits<unsigned int>::min();
-        unsigned int maxY = numeric_limits<unsigned int>::min();
     }
 
-    void Board::updateLimitsH(unsigned int r, unsigned int c, unsigned int l){
-        if(r<minV) this->minV=r;
-        if(r>maxV) this->maxV=r;
-        if(c<minH) this->minH=c;
-        if(c+l>maxH) this->maxH=c+l;
-    }
-    void Board::updateLimitsV(unsigned int r, unsigned int c, unsigned int l){
-        if(r<minV) minV=r;
-        if(r+l>maxV) maxV=r+l;
-        if(c<minH) minH=c;
-        if(c>maxH) maxH=c;
-    }
     void Board::post (unsigned int row, unsigned int col, Direction direction, string data){
         switch (direction)
         {
             case Direction::Vertical:{
-                updateLimitsV(row,col,data.length());
                 unsigned int j = 0;
                 for(unsigned int i=row;i<row+data.length();i++){
                     b[i][col]= data[j];
@@ -44,7 +25,6 @@ namespace ariel {
                 break;
             }
             case Direction::Horizontal:{
-                updateLimitsH(row,col,data.length());
                 unsigned int k = 0;
                 for(unsigned int i=col;i<col+data.length();i++){
                     b[row][i]= data[k];
@@ -55,14 +35,15 @@ namespace ariel {
         }
     }
     string Board::read (unsigned int row, unsigned int col, Direction direction, unsigned int length){
-            string msg = "";
+            string msg;
             switch (direction)
             {
             case Direction::Vertical:
                 for(unsigned int i=row;i<row+length;i++){
                     char c = b[i][col];
-                    if (int(c)== 0)
-                        c='_';
+                    if(c == 0){
+                        c ='_';
+                    }
                     msg+= c;
                 }
                 break;
@@ -70,8 +51,9 @@ namespace ariel {
             case Direction::Horizontal:
                 for(unsigned int i=col;i<col+length;i++){
                     char c = b[row][i];
-                    if (int(c)==0)
+                    if(c == 0){
                         c='_';
+                    }
                     msg+= c;
                 }
                 break;
@@ -82,19 +64,21 @@ namespace ariel {
             return msg;
         }
     void Board::show(){
+        
         time_t now = time(0);
+        const int startYear = 1900;
         tm *ltm = localtime(&now);
         string day = to_string(ltm->tm_mday);
         string month = to_string(ltm->tm_mon);
-        string year = to_string(1900 + ltm->tm_year);
+        string year = to_string(startYear + ltm->tm_year);
         string time = to_string(ltm->tm_hour)+":"+to_string(ltm->tm_min)+":"+to_string(ltm->tm_sec);
-        string file_name = "Board_"+day+"."+month+"."+year+"_"+time+".txt";
+        string file_name = "Board-"+day+"."+month+"."+year+"_"+time+".txt";
         ofstream board(file_name);
-        for(unsigned int i = minV; i<maxV;i++){
-            // board<<"#"<<i<<"\t\t\t\t";
-            for(unsigned int j=minH;j<maxH;j++){
+
+        for(unsigned int i = 0; i<Board::b.size();i++){
+            for(unsigned int j=0;j<Board::b[i].size();j++){
                 char c = b[i][j];
-                if(c==0) c='_';
+                if(c==0) {c='_';}
                 board<<c;
             }
             board<<endl;
--- Board.hpp
+++ Board.hpp
@@ -10,13 +10,6 @@ namespace ariel{
     class Board {
         private:
             map<unsigned int,map<unsigned int, char>> b;
-            unsigned int minV;
-            unsigned int minH;
-            unsigned int maxV;
-            unsigned int maxH;
-
-            void updateLimitsH(unsigned int r, unsigned int c, unsigned int l);
-            void updateLimitsV(unsigned int r, unsigned int c, unsigned int l);
         public:
             Board();
             void post (unsigned int row, unsigned int col, Direction direction, string data);
